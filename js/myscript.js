7c46e999002611008ad15824637dc47e8cdfdfb4
subairifeoluwapo <subairifeoluwapo@gmail.com>
2014-09-07 22:03:56 +0100

    initializing GH-pages

---
+++ js/myscript.js
@@ -0,0 +1,31 @@
+  var toAdd;
+  var toMove;
+
+    $('#addItem').click(function () {
+    	toAdd = $('#input').val();
+    	if (toAdd === '' || toAdd ===  ' ' || toAdd === '  ') {
+    		$('.validInput').append('<p>Enter a valid item name</p>');
+    	} 
+    		else {
+		    	$('.h3').remove();
+		    	$('.p').remove();
+		    	$('.p2').remove();
+		        $('#list').append('<li><input name="checkItem" class="checkbox" type="checkbox">' + toAdd + 
+		        					'        <button id="del">Delete</button></li>');
+    		}
+
+    		$('#input').val('').focus();
+
+    		$('.checkbox').click(function () {
+				toMove = $('#input').val();
+				$("#list2").append('<li><input name="checkItem" class="checkbox" type="checkbox">' + toMove + 
+		        					'        <button id="del">Delete</button></li>');
+			});
+
+			$('#del').click(function() {
+				alert("dfgnkdfg");
+			});
+    });
+
+
+    
