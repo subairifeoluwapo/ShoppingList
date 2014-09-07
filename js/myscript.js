29ea9e0a437095bc77adcb7fdee7efcec8c50236
subairifeoluwapo <subairifeoluwapo@gmail.com>
2014-09-07 22:11:02 +0100

    latest

--- js/myscript.js
+++ js/myscript.js
@@ -12,20 +12,19 @@
 		    	$('.p2').remove();
 		        $('#list').append('<li><input name="checkItem" class="checkbox" type="checkbox">' + toAdd + 
 		        					'        <button id="del">Delete</button></li>');
+		        
     		}
-
-    		$('#input').val('').focus();
-
-    		$('.checkbox').click(function () {
+    			$('.checkbox').click(function () {
 				toMove = $('#input').val();
 				$("#list2").append('<li><input name="checkItem" class="checkbox" type="checkbox">' + toMove + 
 		        					'        <button id="del">Delete</button></li>');
-			});
-
-			$('#del').click(function() {
-				alert("dfgnkdfg");
-			});
+				});
+		        $('#del').click(function() {
+					alert("dfgnkdfg");
+				});
     });
+
+   	$('#input').val('').focus();
 
 
     
