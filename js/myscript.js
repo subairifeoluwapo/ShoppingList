4883c4223775b69f929d5fa2485d692d2cb00ccc
subairifeoluwapo <subairifeoluwapo@gmail.com>
2014-09-08 21:12:58 +0100

    update

--- js/myscript.js
+++ js/myscript.js
@@ -1,31 +1,78 @@
-  var toAdd;
-  var toMove;
-    $('#addItem').click(function () {
-    	event.preventDefault();
-    	toAdd = $('#input').val();
-    	if (toAdd === '' || toAdd ===  ' ' || toAdd === '  ') {
-    		$('.validInput').append('<p>Enter a valid item name</p>');
-    	} 
-    		else {
-		    	$('.h3').remove();
-		    	$('.p').remove();
-		    	$('.p2').remove();
-		        $('#list').append('<li><input name="checkItem" class="checkbox" type="checkbox">' + toAdd + 
-		        					'        <button id="del">Delete</button></li>');
-		        $('#input').val('').focus();
 
-    		}
-    			$('.checkbox').on('click', function () {
-				$('#list2').append('<li><input name="checkItem" class="checkbox" type="checkbox">' + toAdd + 
-		        					'        <button id="del">Delete</button></li>');
-				$('#list').remove();
-				});
 
-		        $('#del').on('click', function() {
-					$('#list2').remove();
-				});
-    });
+	//var toAdd;
+	//var toMove;
+    //$('#addItem').click(function () {
+    	
+    	//toAdd = $('#newItem').val();
+    	//if (toAdd === '' || toAdd ===  ' ' || toAdd === '  ') {
+    		//$('.validInput').text('Enter a valid item name');
+    	//} 
+    		//else {
+		    	//$('.h3').remove();
+		    	//$('.p').remove();
+		    	//$('.p2').remove();
+		        //$('#newlist').append('<li class="listitem"><input name="checkItem" class="checkbox" type="checkbox">' + toAdd + 
+		        					//'        <button class="delete">Delete</button></li>');
+		        //$('#newItem').val('').focus();
 
-   
+		        //$('#newlist').on('click', 'li.listitem', function () {
+				//$('#confirmedlist').append();
+				//});
+    		//}
+    			//$('#newlist').on('click', 'li.listitem', checkoff);
+    			
+    			//$('#list').on('click', 'button.del', deleteItem);
+		        //$('.delete').on('click', function() {
+					//$('#list2').remove();
+				//});
+    //});
 
-    
+var shoppingApp = {
+
+	validate: function() {
+		var item = $("#newItem").val();
+		if ($.trim(item) === "") {
+			alert("Please enter something");
+		} else {
+            return item;
+		}
+	},
+
+	addItem: function() {
+		var item = $("#newItem").val();
+		var checkbox = ('<input type="checkbox">');
+		var del = ('<button id="delete">Delete</button>')
+		if(shoppingApp.validate()) {
+			$('#newlist').append('<li>' + checkbox + " " + newItem + " " + del + '</li>');
+			$('#newItem').val('');
+		}
+		return false;
+	},
+
+	submitItem: function(){
+		$('#add').click(shoppingApp.addItem());
+	},
+
+	deleteItem: function(event) {
+		event.preventDefault();
+		$(this).parent().remove();
+	},
+
+	switching: function(event) {
+		event.preventDefault();
+		if($(this).is(':checked')) {
+			$(this).parent().appendTo('#confirmedlist');
+		} else {
+			$(this).parent().appendTo('#newlist')
+		}
+	},
+
+	initialize: function() {
+		$('#add').click(shoppingApp.addItem);
+		$('.welcome ul').on('change', 'input[type=checkbox]', shoppingApp.switching);
+		$('.welcome ul').on('click', 'button', shoppingApp.deleteItem)
+	},
+
+}
+$(document).ready(shoppingApp.initialize);
